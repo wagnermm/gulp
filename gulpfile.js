7fda01099263d3c498981cf436fe32c90cb76b19
Wagner Miranda <wagnermarquesdemiranda@gmail.com>
2023-01-30 16:04:25 -0300

    renomeando pasta img. unificando arquivos css/js com gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -7,23 +7,36 @@ const image = require('gulp-image')
 
 
 function tarefasCSS(cb) {
-    return gulp.src('./vendor/**/*.css')
-        .pipe(concat('libs.css'))
+    return gulp.src([
+        'node_modules/bootstrap/dist/css/bootstrap.css',
+        './vendor/owl/css/owl.css',
+        './node_modules/font-awesome/css/font-awesome.css',
+        './vendor/jquery-ui/jquery-ui.css',
+        './src/css/style.css'
+    ])
+        .pipe(concat('styles.css'))
         .pipe(cssmin())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('./dist/css'))
 }
 
 function tarefasJS() {
-    return gulp.src('./vendor/**/*.js')
-        .pipe(concat('libs.js'))
+    return gulp.src([
+        './node_modules/jquery/dist/jquery.js',
+        './node_modules/bootstrap/dist/js/bootstrap.js',
+        './vendor/owl/js/owl.js',
+        './vendor/jquery-mask/jquery.mask.js',
+        './vendor/jquery-ui/jquery-ui.js',
+        './src/js/script.js'
+    ])
+        .pipe(concat('scripts.js'))
         .pipe(uglify())
         .pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('./dist/js'))
 }
 
 function tarefasImagem() {
-    return gulp.src('./src/img/*')
+    return gulp.src('./src/images/*')
         .pipe(image({
             pngquant: true,
             optipng: false,
